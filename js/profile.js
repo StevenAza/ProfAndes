ae7f3fe53284dc3451f228e596c036ec47382d36
Steven Amado <nazamado569@gmail.com>
2020-07-08 18:11:15 -0500

    :art: Posicion arriba - izquierda en Tema Material

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function() {
 
     var element = jQuery(".timeline__year");
+    var sticky = new Sticky('.cont__menu');
     jQuery(".timeline__year").click(function () {
       jQuery(this).parent().find(".timeline__content").toggleClass("oculta_anio");
     });
@@ -17,7 +18,7 @@ jQuery(document).ready(function() {
       },
       slidesPerView:4
     });
-    switch (localStorage.getItem("posicion")) {
+    switch (localStorage.getItem("posicion_profile")) {
       case "arriba":
         jQuery(".main_teach").addClass("pr_item__top");
         jQuery(".main_teach").removeClass("pr_item__left pr_item__right");
@@ -31,14 +32,42 @@ jQuery(document).ready(function() {
         jQuery(".main_teach").removeClass("pr_item__top pr_item__left");
         break; 
     }
-
+    switch (localStorage.getItem("tema_profile")) {
+      case "material":
+        jQuery(".main_teach").addClass("prof_style__material");
+        jQuery(".main_teach").removeClass("prof_style__classic prof_style__dark");
+        break;
+      case "clasico":
+        jQuery(".main_teach").addClass("prof_style__classic");
+        jQuery(".main_teach").removeClass("prof_style__material prof_style__dark");
+        break;
+      case "oscuro":
+        jQuery(".main_teach").addClass("prof_style__dark");
+        jQuery(".main_teach").removeClass("prof_style__material prof_style__classic");
+        break;  
+         
+    }
+ 
+    if(jQuery(".main_teach.prof_style__material").length > 0) {
+      rippleCustom(".action_mas__btn");
+     rippleCustom(".content-menu-filter .row_tab");
+    }
     if(jQuery(".main_teach.pr_item__top").length  > 0) {
-       var sticky = new Sticky('.cont__menu');
         jQuery(".main_teach .cont__header").removeClass("col-md-3 col-lg-4").addClass("col-md-12 col-lg-12");
         jQuery(".main_teach .con__information").removeClass("col-xs-11 col-sm-9 col-md-8 col-lg-8").addClass("col-xs-12 col-sm-12 col-md-12 col-lg-12");
     }
 
     if(jQuery(".main_teach.pr_item__left").length  > 0) {
-    var sticky = new Sticky('.prf_header--contenedor');
+      var sticky = new Sticky('.prf_header--contenedor');
     }
 });
+
+function rippleCustom(elem) {
+  window.rippler = $.ripple(elem, {
+    debug: true,
+    multi: true
+  });
+}
+
+
+
